a4ff93a6dfeeb543d8fdc20184e4298b411932c4
Dago Serge-Martial <46801097+Dagoserge-martial@users.noreply.github.com>
2026-01-13 19:32:53 +0000

    logo1

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,6 +1,6 @@
 <template>
     <img 
-        src="/storage/logos/logo2.svg" 
+        src="/logos/logo2.svg" 
         alt="Logo"
         class="h-full w-auto object-contain"
         style="max-width: min(100px, 100%); max-height: 100%;"
